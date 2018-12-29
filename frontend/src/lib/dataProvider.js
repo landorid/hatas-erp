377a429b9ea3f2f831365e24e318c138e0e13b06
Lándori Dávid <landori.david@gmail.com>
2018-12-29 01:31:14 +0100

    Migrate Apollo Boost to Apollo Client

--- frontend/src/lib/dataProvider.js
+++ frontend/src/lib/dataProvider.js
@@ -1,21 +1,42 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 import { endpoint } from '../config';
 
-const client = new ApolloClient({
-  uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
-  request: operation => {
-    operation.setContext({
-      fetchOptions: {
-        credentials: 'include',
-      },
-    });
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'cache-and-network',
+    errorPolicy: 'all',
   },
-});
-
-client.defaultOptions = {
   query: {
     fetchPolicy: 'cache-and-network',
-    partialRefetch: true,
+    errorPolicy: 'all',
+  },
+  mutate: {
+    errorPolicy: 'all',
   },
 };
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+      credentials: 'include'
+    })
+  ]),
+  cache: new InMemoryCache(),
+  defaultOptions,
+});
+
 export default client;
