ffe6bd3d3cff550c3217dd2ff739765180f90811
Lándori Dávid <info@honlapvarazslo.com>
2019-12-01 12:13:41 +0100

    Just admin can create new user

--- frontend/src/containers/Users.js
+++ frontend/src/containers/Users.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import UsersListing from '../components/table/UsersListing';
-import PageTitle from '../components/PageTitle';
-import AddFab from '../components/AddFab';
-import TableLoading from '../components/table/elements/TableLoading';
-import Chip from './Worksheets';
-import { tableLabels } from '../config';
+import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import UsersListing from '../components/table/UsersListing'
+import PageTitle from '../components/PageTitle'
+import AddFab from '../components/AddFab'
+import TableLoading from '../components/table/elements/TableLoading'
+import User from '../components/User'
+import Header from '../components/Header'
+import { hasPermission } from '../lib/utils'
 
 const USERS_QUERY = gql`
   query USERS_QUERY {
@@ -27,79 +28,39 @@ const USERS_QUERY = gql`
       avatar
     }
   }
-`;
+`
 
-const Users = (props) => {
-  const { history } = props;
-
-  const columns = [
-    {
-      name: 'ID',
-      display: 'excluded',
-      options: {
-        display: 'excluded',
-        filter: false,
-      },
-    },
-    {
-      name: 'Vezetéknév',
-      display: 'excluded',
-      options: {
-        display: 'excluded',
-        filter: false,
-      },
-    },
-    {
-      name: 'Név',
-    },
-    {
-      name: 'Beosztás',
-      options: {
-        customBodyRender: (value) => value.name,
-      },
-    },
-    {
-      name: 'Státusz',
-      options: {
-        customBodyRender: (value) => {
-          return value;
-        },
-      },
-    },
-  ];
-
-  const options = {
-    selectableRows: false,
-    fixedHeader: true,
-    print: false,
-    onRowClick: (currentRowsSelected) => {
-      history.push(`/worksheet/${currentRowsSelected[0]}`);
-    },
-    ...tableLabels,
-  };
-
+const Users = () => {
   const rows = [
     { id: 'lastName', numeric: false, label: 'Név' },
     { id: 'job', numeric: false, label: 'Beosztás' },
     { id: 'permissions', numeric: false, label: 'Jogosultság' },
     { id: 'status', numeric: false, label: 'Státusz' },
-  ];
+  ]
 
   return (
     <div>
       <PageTitle title="Felhasználók"/>
       <Query query={USERS_QUERY} fetchPolicy="cache-first">
         {({ data: { users }, loading }) => {
-          if (loading) return <TableLoading/>;
+          if (loading) return <TableLoading/>
           return (
             <UsersListing data={users} rows={rows}/>
-          );
+          )
         }}
       </Query>
-
-      <AddFab title="Új felhasználó" to="/users/add"/>
+      <User>
+        {({ data, error, loading }) => {
+          return (
+            !loading && !error && data.me &&
+            hasPermission(data.me.permissions, ['ADMIN']) ?
+              <AddFab title="Új felhasználó"
+                      to="/users/add"/> : null
+          )
+        }}
+      </User>
     </div>
-  );
-};
+  )
+}
 
-export default Users;
+export default Users
